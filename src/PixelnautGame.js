90aa66713bf55eaf2967338e07e5ebe7073d088e
Fuzzy Yeti <fuzzyyeti@github.com>
2022-03-13 13:16:53 -0600

    update url with env var

--- src/PixelnautGame.js
+++ src/PixelnautGame.js
@@ -1,10 +1,19 @@
 import SelectOrcanaut from "./SelectOrcanaut";
 import FishTank from "./FishTank";
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
 const PixelnautGame = () =>
 {
     const fishTank = useRef(null);
+    useEffect(() => {
+        if(fishTank) {
+            fishTank.current.contentWindow.window.godot_ready = () => {
+                console.log('godot ready');
+                console.log('url', process.env.REACT_APP_GODOT_URL);
+                fishTank.current.contentWindow.window.add_api_url(process.env.REACT_APP_GODOT_URL);
+            }
+        }
+    },[fishTank])
     return (<>
         <SelectOrcanaut fishTank={fishTank}/>
         <FishTank ref={fishTank}/>
